271675001462382d5e140f66c60e0788f709da33
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-07-04 08:13:52 -0300

    getting user data

--- app/src/main/java/br/com/fiap/userdata/MainActivity.kt
+++ app/src/main/java/br/com/fiap/userdata/MainActivity.kt
@@ -1,29 +1,40 @@
 package br.com.fiap.userdata
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.fiap.userdata.ui.theme.UserDataTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             UserDataTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    Greeting()
                 }
             }
         }
@@ -31,17 +42,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+    
+    var text by remember {
+        mutableStateOf("")
+    }
+    
+    Column (
+        modifier = Modifier.
+                    padding(32.dp)
+    ) {
+        Text(text = text)
+        TextField(
+            value = text,
+            label = {
+                Text(text = "Digite o nome")
+            },
+            placeholder = {
+                Text(text = "Digite aqui")
+            },
+            trailingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.settings_24),
+                    contentDescription = "settings icon"
+                )
+            },
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.person_24),
+                    contentDescription = "user icon"
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth(),
+            onValueChange = { value ->
+                text = value
+            }
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     UserDataTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
